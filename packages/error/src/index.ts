2136a70117cff0b6cc1a6ed7f91168c159a6f2d1
cdziv <codingziv@gmail.com>
2025-04-04 17:06:43 +0800

    test: this commit should push fail

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,4 +1,2 @@
 export * from './lib/error-with-code';
 export * from './lib/create-error-codes';
-
-const a: string = 123;
--- packages/error/src/lib/create-error-codes.spec.ts
+++ packages/error/src/lib/create-error-codes.spec.ts
@@ -38,4 +38,7 @@ describe('createErrorCodes', () => {
       ERROR_2: 'MODULE.ERROR_2',
     });
   });
+  it('fail', () => {
+    expect(1).toBe(2);
+  });
 });

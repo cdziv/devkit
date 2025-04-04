3b4646f2e1a1ae1aa6254356343f09b611c006e1
cdziv <codingziv@gmail.com>
2025-04-04 16:58:03 +0800

    chore: setup git hooks(this commit should push fail)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,6 +27,14 @@ export default [
           ],
         },
       ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
   {
--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/error-with-code';
 export * from './lib/create-error-codes';
+
+const a: string = 123;

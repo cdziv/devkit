712360aafabadc722bd9c8182568899fd4957b89
cdziv <codingziv@gmail.com>
2025-04-04 18:19:47 +0800

    fix: infer types

--- packages/error/src/lib/create-error-codes.spec.ts
+++ packages/error/src/lib/create-error-codes.spec.ts
@@ -3,25 +3,29 @@ import { createErrorCodes } from './create-error-codes';
 
 describe('createErrorCodes', () => {
   it('should create error codes from an array', () => {
-    const errorCodes = createErrorCodes(['ERROR_1', 'ERROR_2']);
+    const source = ['ERROR_1', 'ERROR_2'] as const;
+    const errorCodes = createErrorCodes(source);
     expect(errorCodes).toEqual({
       ERROR_1: 'ERROR_1',
       ERROR_2: 'ERROR_2',
     });
   });
   it('should create error codes from an object', () => {
-    const errorCodes = createErrorCodes({
+    const source = {
       ERROR_1: 'ERROR_1',
       ERROR_2: 'ERROR_2',
-    });
+    } as const;
+    const errorCodes = createErrorCodes(source);
     expect(errorCodes).toEqual({
       ERROR_1: 'ERROR_1',
       ERROR_2: 'ERROR_2',
     });
   });
   it('should create error codes with a module name', () => {
-    const errorCodes = createErrorCodes(['ERROR_1', 'ERROR_2'], {
-      moduleName: 'MODULE',
+    const source = ['ERROR_1', 'ERROR_2'] as const;
+    const moduleName = 'MODULE' as const;
+    const errorCodes = createErrorCodes(source, {
+      moduleName,
     });
     expect(errorCodes).toEqual({
       ERROR_1: 'MODULE/ERROR_1',
@@ -29,9 +33,12 @@ describe('createErrorCodes', () => {
     });
   });
   it('should create error codes with a custom delimiter', () => {
-    const errorCodes = createErrorCodes(['ERROR_1', 'ERROR_2'], {
-      moduleName: 'MODULE',
-      delimiter: '.',
+    const moduleName = 'MODULE' as const;
+    const source = ['ERROR_1', 'ERROR_2'] as const;
+    const delimiter = '.' as const;
+    const errorCodes = createErrorCodes(source, {
+      moduleName,
+      delimiter,
     });
     expect(errorCodes).toEqual({
       ERROR_1: 'MODULE.ERROR_1',
--- packages/error/src/lib/create-error-codes.ts
+++ packages/error/src/lib/create-error-codes.ts
@@ -1,6 +1,49 @@
+/**
+ * Creates a mapping of error codes based on the provided source and options.
+ *
+ * This utility function allows you to generate error codes in a structured format
+ * by appending a module name and a delimiter to each key or value in the source.
+ *
+ * **NOTE**: Should provide source and options as `const` to ensure type safety.
+ *
+ * @param source - The source for generating error codes. It can be:
+ *                 - An array of strings, where each string represents a key.
+ *                 - An object where each key-value pair represents a mapping.
+ * @param options - Optional configuration for generating error codes.
+ *                  - `moduleName` (string): A prefix to be added to each error code.
+ *                  - `delimiter` (string): A delimiter to separate the module name and the key/value.
+ *
+ * @returns A mapping of error codes where:
+ *          - For an array source, each key is mapped to a string in the format `moduleName/delimiter/key`.
+ *          - For an object source, each key is mapped to a string in the format `moduleName/delimiter/value`.
+ *
+ * @example
+ * // Using an array as the source
+ * const source = ['NOT_FOUND', 'INVALID'] as const;
+ * const errorCodes = createErrorCodes(source, { moduleName: 'USER', delimiter: '-' });
+ * // Result: { NOT_FOUND: 'USER-NOT_FOUND', INVALID: 'USER-INVALID' }
+ *
+ * @example
+ * // Using an object as the source
+ * const source = { notFound: 'NOT_FOUND', invalid: 'INVALID' } as const;
+ * const errorCodes = createErrorCodes(source, { moduleName: 'USER' });
+ * // Result: { notFound: 'USER/NOT_FOUND', invalid: 'USER/INVALID' }
+ *
+ * @example
+ * // Using custom module name and delimiter
+ * const source = ['NOT_FOUND', 'INVALID'] as const;
+ * const moduleName = 'USER' as const;
+ * const delimiter = '.' as const;
+ * const errorCodes = createErrorCodes(source, { moduleName, delimiter });
+ * // Result: { NOT_FOUND: 'USER.NOT_FOUND', INVALID: 'USER.INVALID' }
+ */
 export function createErrorCodes<
-  T extends { [key: string]: string } | Array<string>
->(source: T, options?: CreateErrorCodesOptions): ErrorCodes<T> {
+  T extends readonly string[] | { [key: string]: string },
+  Options extends CreateErrorCodesOptions = CreateErrorCodesOptions
+>(
+  source: T,
+  options?: Options
+): ErrorCodes<T, ModuleNameOfOptions<Options>, DelimiterOfOptions<Options>> {
   const { moduleName = '', delimiter = '/' } = options || {};
 
   if (Array.isArray(source)) {
@@ -9,31 +52,37 @@ export function createErrorCodes<
       return acc;
     }, {} as Record<string, string>) as ErrorCodes<T>;
   }
-
   return Object.entries(source).reduce((acc, [key, value]) => {
     acc[key] = moduleName ? `${moduleName}${delimiter}${value}` : value;
     return acc;
   }, {} as Record<string, string>) as ErrorCodes<T>;
 }
 
+type DefaultDelimiter = '/';
 type ErrorCodeValue<
   T extends string,
   Module extends string = '',
-  Delimiter extends string = '/'
+  Delimiter extends string = DefaultDelimiter
 > = Module extends '' ? `${T}` : `${Module}${Delimiter}${T}`;
 type ErrorCodes<
-  T,
+  T extends readonly string[] | { [key: string]: string },
   Module extends string = '',
-  Delimiter extends string = '/'
+  Delimiter extends string = DefaultDelimiter
 > = T extends { [key: string]: string }
   ? {
       [K in keyof T]: ErrorCodeValue<T[K], Module, Delimiter>;
     }
-  : T extends Array<string>
+  : T extends readonly string[]
   ? {
       [K in T[number]]: ErrorCodeValue<K, Module, Delimiter>;
     }
   : never;
+type ModuleNameOfOptions<T extends CreateErrorCodesOptions> =
+  T['moduleName'] extends string ? T['moduleName'] : '';
+type DelimiterOfOptions<
+  T extends CreateErrorCodesOptions,
+  D extends string = DefaultDelimiter
+> = T['delimiter'] extends string ? T['delimiter'] : D;
 
 export interface CreateErrorCodesOptions {
   moduleName?: string;

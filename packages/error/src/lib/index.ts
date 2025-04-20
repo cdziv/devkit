828f1419943548631dc25911a136cf8dfc4e0458
cdziv <codingziv@gmail.com>
2025-04-20 17:16:33 +0800

    ref(error): Extract the interfaces into a separate file.

---
+++ packages/error/src/lib/index.ts
@@ -0,0 +1,3 @@
+export { ErrorWithCode } from './error-with-code';
+export { createErrorCodes } from './create-error-codes';
+export type { ErrorCodes, CreateErrorCodesOptions } from './interfaces';
